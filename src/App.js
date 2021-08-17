df145555f1092774b808f743106878770b9e6c52
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-08-17 15:16:13 -0400

    Arrays are printed in the console.log

--- src/App.js
+++ src/App.js
@@ -1,16 +1,46 @@
 import './App.css';
-import './components/sort';
+import React,{Component} from "react";
+
+// with help from https://www.youtube.com/watch?v=Ci-6NYmaimw
 
 
-function App() {
+class App extends Component{
+    state = {
+        array: [],
+        arraySteps: [],
+        colourKey: [],
+        colourSteps: [],
+        count: 10,
+        delay: 100,
+        algorithm: '',
+        timeouts: [],
+    };
 
+    componentDidMount() {
+        this.generateRandomArray();
+    }
 
-  return (
+    generateRandomInt = (min,max)=>{
+        return Math.floor(Math.random()*(max-min)+min);
+    }
+    generateRandomArray=()=>{
+        const count = this.state.count;
+        const temp = [];
 
-    <div className="App">
-      <h1>Matthew</h1>
-    </div>
-  );
+        for(let i = 0; i<count; i++){
+            temp.push(this.generateRandomInt(50,200));
+        }
+        console.log(temp);
+    }
+    render(){
+
+        return(
+            <div className="App">
+                <h1>Hulloo</h1>
+            </div>
+
+        );
+    }
 }
 
 export default App;
