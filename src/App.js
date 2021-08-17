90a2578a0413b6f5b41d181fbbf9579b4cf87027
18mpr1 <matthew.paul.rieckenberg@gmail.com>
2021-08-17 15:01:39 -0400

    Reformatted it.

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ function App() {
 
 
   return (
+
     <div className="App">
+      <h1>Matthew</h1>
     </div>
   );
 }
--- src/algorithms/bubblesort.jsx
+++
@@ -1,9 +0,0 @@
-import '/src/App';
-
-function BubbleSort(array){
-    console.log("Bubble Sort");
-
-
-}
-
-export default BubbleSort();
